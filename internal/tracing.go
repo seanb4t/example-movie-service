c5ce3b0ac420713a3ae981730e5c6b2073bbbca4
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2023-09-02 07:28:29 -0400

    feat: otel resource config updates

--- internal/tracing.go
+++ internal/tracing.go
@@ -3,18 +3,33 @@ package internal
 import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 var Tracer = otel.Tracer("github.com/seanb4t/example-movie-service")
 
 func InitTracer() (*trace.TracerProvider, error) {
+	r, err := resource.Merge(resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("Movie Service"),
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
 	//exporter, err := stdout.New(stdout.WithPrettyPrint())
 	//if err != nil {
 	//	return nil, err
 	//}
+
 	tp := trace.NewTracerProvider(
 		trace.WithSampler(trace.AlwaysSample()),
+		trace.WithResource(r),
 		//sdktrace.WithBatcher(exporter),
 	)
 	otel.SetTracerProvider(tp)
